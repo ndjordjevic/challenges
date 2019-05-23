5edecb7d763983ee0fd550c084ae128ddec46974
ndjordjevic <ndjordjevic@gmail.com>
2019-05-23 22:35:20 +0200

    day_4

---
+++ DAY_4/sortArrayByParity.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sortArrayByParity([]int{3, 1, 2, 4}))
+}
+
+func sortArrayByParity(A []int) []int {
+	var r []int
+
+	for _, e := range A {
+		if e%2 == 0 {
+			// insert at the begging (prepend)
+			r = append([]int{e}, r...)
+		} else {
+			r = append(r, e)
+		}
+	}
+
+	return r
+}
