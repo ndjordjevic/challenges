80e6ee4860f45cb46d065cc6692e466d9954abf1
ndjordjevic <ndjordjevic@gmail.com>
2019-11-11 11:11:16 +0100

    middleNode and reverseList

---
+++ Lists/leet_middleNode/main.go
@@ -0,0 +1,23 @@
+package main
+
+// ListNode node
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+func middleNode(head *ListNode) *ListNode {
+	fast := head
+	slow := head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
---
+++ Lists/leet_reverseList/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	var head = new(ListNode)
+	head.Val = 1
+	head.Next = &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: nil}}}
+
+	reversed := reverseList2(head)
+
+	for reversed != nil {
+		fmt.Printf("%v\n", reversed)
+		reversed = reversed.Next
+	}
+}
+
+// ListNode list node
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var list *ListNode
+
+	for head != nil {
+		list = &ListNode{Val: head.Val, Next: list}
+		head = head.Next
+	}
+
+	return list
+}
+
+func reverseList2(head *ListNode) *ListNode {
+	var reverse func(head, tail *ListNode) *ListNode
+	reverse = func(head, tail *ListNode) *ListNode {
+		if head == nil {
+			return tail
+		}
+		next := head.Next
+		head.Next = tail
+		return reverse(next, head)
+	}
+	return reverse(head, nil)
+}
