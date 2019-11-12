4a5c92a282b8bc28eeac3f1429a6d392fca0f686
ndjordjevic <ndjordjevic@gmail.com>
2019-11-12 13:24:31 +0100

    dummy node merge two lists

--- Lists/leet_mergeTwoLists/main.go
+++ Lists/leet_mergeTwoLists/main.go
@@ -9,9 +9,44 @@ type ListNode struct {
 
 func main() {
 	head := &ListNode{}
-	createList(head, 1, 2, 3, 4)
-	printList(head)
-	printList(head)
+	head1 := &ListNode{}
+
+	createList(head, 1, 2, 4)
+	createList(head1, 1, 3, 4)
+
+	printList(mergeTwoLists(head, head1))
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	// base case
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	// dummyNode method
+	dummy := &ListNode{0, nil}
+	var dummyP *ListNode = dummy
+	var p1 *ListNode = l1
+	var p2 *ListNode = l2
+
+	for p1 != nil && p2 != nil {
+		if p1.Val <= p2.Val {
+			dummyP.Next = p1
+			p1 = p1.Next
+		} else {
+			dummyP.Next = p2
+			p2 = p2.Next
+		}
+		dummyP = dummyP.Next
+	}
+
+	// ternary operator
+	dummyP.Next = map[bool]*ListNode{true: p1, false: p2}[p1 != nil]
+
+	return dummy.Next
 }
 
 func printList(head *ListNode) {
