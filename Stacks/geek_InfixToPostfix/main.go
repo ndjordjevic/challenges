97c38a121f10fa1fdbedca999bbac5cf6b8b0daf
ndjordjevic <ndjordjevic@gmail.com>
2019-11-13 13:09:50 +0100

    exp infix to postfix

---
+++ Stacks/geek_InfixToPostfix/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ndjordjevic/challenges/Stacks/stack"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	fmt.Println(infixToPostFix("a+b*c+d"))
+}
+
+func infixToPostFix(s string) string {
+	var sb strings.Builder
+
+	stk := &stack.Stack{}
+
+	for _, value := range s {
+		if unicode.IsLetter(value) || unicode.IsDigit(value) {
+			sb.WriteRune(value)
+		} else if value == '(' {
+			stk.Push(value)
+		} else if value == ')' {
+			for !stk.IsEmpty() && stk.Peek() != '(' {
+				v, err := stk.Pop()
+				if err != nil {
+					panic(err)
+				}
+				sb.WriteRune(v.(rune))
+			}
+
+			if !stk.IsEmpty() && stk.Peek() != '(' {
+				return "Invalid expression"
+			} else {
+				stk.Pop()
+			}
+		} else {
+			for !stk.IsEmpty() && prec(value) <= prec(stk.Peek().(rune)) {
+				if stk.Peek() == '(' {
+					return "Invalid Expression"
+				}
+
+				v, err := stk.Pop()
+				if err != nil {
+					panic(err)
+				}
+				sb.WriteRune(v.(rune))
+			}
+
+			stk.Push(value)
+		}
+	}
+
+	for !stk.IsEmpty() {
+		if stk.Peek() == '(' {
+			return "Invalid expression"
+		}
+
+		v, err := stk.Pop()
+		if err != nil {
+			panic(err)
+		}
+		sb.WriteRune(v.(rune))
+	}
+
+	return sb.String()
+}
+
+func prec(r rune) int {
+	switch r {
+	case '+', '-':
+		return 1
+	case '*', '/':
+		return 2
+	case '^':
+		return 3
+	default:
+		return -1
+	}
+}
