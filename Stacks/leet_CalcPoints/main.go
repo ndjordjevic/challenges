87d982cbe8d397e059e5df54c2141866e3476e33
ndjordjevic <ndjordjevic@gmail.com>
2019-11-13 19:39:37 +0100

    leet calc points

---
+++ Stacks/leet_CalcPoints/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(calPoints([]string{"5", "-2", "4", "C", "D", "9", "+", "+"}))
+}
+
+func calPoints(ops []string) int {
+	sum, val, stack := 0, 0, make([]int, 0)
+	for _, op := range ops {
+		switch op {
+		case "C":
+			val := stack[len(stack)-1]
+			sum -= val
+			stack = stack[:len(stack)-1]
+		case "D":
+			val := stack[len(stack)-1] * 2
+			sum += val
+			stack = append(stack, val)
+		case "+":
+			val = stack[len(stack)-1] + stack[len(stack)-2]
+			sum += val
+			stack = append(stack, val)
+		default:
+			val, _ := strconv.Atoi(op) // ignore error
+			stack = append(stack, val)
+			sum += val
+		}
+	}
+	return sum
+}
