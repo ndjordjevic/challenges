d4f6703fc8117be80c434f891ad1f80205759a6e
e1082760 <nele2345>
2019-11-20 09:29:45 +0100

    ctci fibonacci

---
+++ Recursion/ctci_Fibonacci/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fibonacci2(1))
+}
+
+func fibonacci(i int) int {
+	if i == 0 {
+		return 0
+	}
+
+	if i == 1 {
+		return 1
+	}
+
+	return fibonacci(i-1) + fibonacci(i-2)
+}
+
+func fibonacci2(n int) int {
+	a := make([]int, n+1)
+	return fib(n, a)
+}
+
+func fib(i int, memo []int) int {
+	if i == 0 || i == 1 {
+		return i
+	}
+
+	if memo[i] == 0 {
+		memo[i] = fib(i-1, memo) + fib(i-2, memo)
+	}
+
+	return memo[i]
+}
+
+func fibonacci3(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	a, b := 0, 1
+	for i := 2; i < n; i++ {
+		c := a + b
+		a = b
+		b = c
+	}
+	return a + b
+}
