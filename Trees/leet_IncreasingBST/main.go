0fad78fcb4b356114642a5faa6f1ba51e80b7d8b
ndjordjevic <ndjordjevic@gmail.com>
2019-11-20 18:11:39 +0100

    leet increasing bst unsolved

--- Trees/leet_IncreasingBST/main.go
+++ Trees/leet_IncreasingBST/main.go
@@ -12,38 +12,141 @@ type TreeNode struct {
 }
 
 func main() {
-	var t TreeNode
-	createBSTFromSlice([]string{"5", "3", "6", "2", "4", "", "8", "1", "", "", "", "7", "9"}, &t)
-	inOrder(&t)
+	//var t TreeNode
+	//a := createBSTFromSlice([]string{"1", "2", "3", "4", "5", "6"}, &t)
+	////printLevelOrder(a)
+
+	root := &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val:   1,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val:   4,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+		Right: &TreeNode{
+			Val:  6,
+			Left: nil,
+			Right: &TreeNode{
+				Val: 8,
+				Left: &TreeNode{
+					Val:   7,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val:   9,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+		},
+	}
+
+	r := increasingBST(root)
+	printInOrder(r)
 }
 
-func createBSTFromSlice(strings []string, root *TreeNode) {
-	insertLevelOrder(strings, root, 0)
+func increasingBST(root *TreeNode) *TreeNode {
+	dummy := &TreeNode{}
+	prev := dummy
+
+	inorder(root, prev)
+
+	prev.Left = nil
+	prev.Right = nil
+	//ret:=dummy.Right
+
+	return prev.Right
 }
 
-func insertLevelOrder(a []string, root *TreeNode, i int) {
-	val, err := strconv.Atoi(a[i])
-	if err != nil {
+func inorder(curr *TreeNode, prev *TreeNode) {
+	if curr == nil {
 		return
 	}
 
-	if i < len(a) {
+	inorder(curr.Left, prev)
+	prev.Left = nil
+	prev.Right = curr
+	prev = curr
+	inorder(curr.Right, prev)
+}
+
+func printInOrder(root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	printInOrder(root.Left)
+	fmt.Println(root.Val)
+	printInOrder(root.Right)
+}
+
+func createBSTFromSlice(strings []string, root *TreeNode) *TreeNode {
+	return insertLevelOrder(strings, root, 0)
+}
+
+func insertLevelOrder(arr []string, root *TreeNode, i int) *TreeNode {
+	if i < len(arr) {
+		val, err := strconv.Atoi(arr[i])
+
+		if err != nil {
+			return nil
+		}
+
 		t := &TreeNode{Val: val}
 		root = t
 
-		insertLevelOrder(a, root.Left, 2*i+1)
-		insertLevelOrder(a, root.Right, 2*i+2)
+		root.Left = insertLevelOrder(arr, root.Left, 2*i+1)
+		root.Right = insertLevelOrder(arr, root.Right, 2*i+2)
 	}
+
+	return root
 }
 
-func inOrder(root *TreeNode) {
-	if root != nil {
-		inOrder(root.Left)
+func printLevelOrder(root *TreeNode) {
+	h := height(root)
+
+	for i := 0; i < h; i++ {
+		printGivenLevel(root, i)
+	}
+
+}
+
+func printGivenLevel(root *TreeNode, i int) {
+	if root == nil {
+		return
+	}
+	if i == 0 {
 		fmt.Println(root.Val)
-		inOrder(root.Right)
+	} else if i > 0 {
+		printGivenLevel(root.Left, i-1)
+		printGivenLevel(root.Right, i-1)
 	}
 }
 
-//func increasingBST(root *TreeNode) *TreeNode {
-//
-//}
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0
+	} else {
+		lHeight := height(root.Left)
+		rHeight := height(root.Right)
+
+		if lHeight > rHeight {
+			return lHeight + 1
+		} else {
+			return rHeight + 1
+		}
+	}
+}
